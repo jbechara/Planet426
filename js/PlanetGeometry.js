76497616e29ffd87c9c2775652228312f3438f24
Jad Bechara <jad@nat-oitwireless-inside-vapornet100-d-8778.Princeton.EDU>
2018-05-12 01:59:03 -0400

    Fixed index.html

--- js/PlanetGeometry.js
+++ js/PlanetGeometry.js
@@ -1,7 +1,6 @@
 // A wrapper of IcosahedronGeometry for planetoids
 
 // PlanetGeometry
-
 function PlanetGeometry(radius, detail) {
     THREE.Geometry.call(this);
 
@@ -14,16 +13,22 @@ function PlanetGeometry(radius, detail) {
  	this.fromBufferGeometry(new THREE.IcosahedronBufferGeometry(radius, detail));
     this.mergeVertices();
 
+    console.log(this);
+
     return this;
 }
 
 PlanetGeometry.prototype = Object.create(THREE.Geometry.prototype);
 
+PlanetGeometry.prototype.generateAngleMap = function() {
+
+}
+
 PlanetGeometry.prototype.applyHeightMap = function() {
     var verts = this.vertices;
     var r = this.parameters.radius;
+
     for (var i = 0; i < verts.length; i++) {
-        verts[i].normalize().multiplyScalar(r);
+        verts[i].normalize().multiplyScalar();
     }
-    // TODO: Why does this work without update flags? :/
 };
